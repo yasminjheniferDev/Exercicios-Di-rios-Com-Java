cfebd7aca776d3ab594f45d3236ca8d4f6b400e8
YASMIN JHENIFER <155997837+yasminjheniferDev@users.noreply.github.com>
2026-09-04 18:34:47 -0300

    + 1 exercício

--- Exercicios 3/NImparesFor.java
+++ Exercicios 3/NImparesFor.java
@@ -1,22 +1,22 @@
+public class NImparesFor {
+        public static void main(String[] args) {
 
-public class NParesFor {
-    public static void main(String[] args) {
 
-
-        // Laço imprimindo números entre 0 e 100 + incremento a cada 2 números. Números pares.
-        for (int c = 0; c <= 100; c += 2) {
-            System.out.println(c);
-        }
-
-        System.out.println();
-        System.out.println();
-
-        // Laço imprimindo números entre 0 e 100 + condição se é par.
-        for (int c = 0; c <= 100; c++) {
-            if (c % 2 == 0) {
+            // Laço imprimindo números entre 1 e 100 + incremento a cada 2 números. Números impares.
+            for (int c = 1; c <= 100; c+=2) {
                 System.out.println(c);
+            }
 
+            System.out.println();
+            System.out.println();
+
+            // Laço imprimindo números entre 1 e 100 + condição se é ímpar.
+            for (int c = 1; c <= 100; c++) {
+                if (c % 2 == 1) {
+                    System.out.println(c);
+
+                }
             }
         }
     }
-}
+
