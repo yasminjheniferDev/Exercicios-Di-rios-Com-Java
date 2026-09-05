6af7c06b0899fe91f35c710a5d44511aa15aac6a
YASMIN JHENIFER <155997837+yasminjheniferDev@users.noreply.github.com>
2026-09-05 12:25:31 -0300

    + 1 Exercício

---
+++ Exercicios 3/ParesDe1Ate50.java
@@ -0,0 +1,13 @@
+public class ParesDe1Ate50 {
+    public static void main(String[] args) {
+
+        //Laço imprimindo os números de 1 até 50 com a condição de que sejam números pares.K
+        for (int cont = 1; cont <= 50; cont++) {
+            if (cont % 2 == 0) {
+                System.out.println(cont);
+
+            }
+        }
+
+    }
+}
