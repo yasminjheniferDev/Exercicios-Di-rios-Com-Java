f6b7b953528756e96e54c14d10bf1607f976112d
YASMIN JHENIFER <155997837+yasminjheniferDev@users.noreply.github.com>
2026-09-05 14:08:44 -0300

    + 1 Exercício

---
+++ Exercicios 3/SomaDe1Ate100.java
@@ -0,0 +1,12 @@
+public class SomaDe1Ate100 {
+    public static void main(String[] args) {
+
+        //Laço contando todos os números de 1 até 100, e imprimindo a soma de todos os números no finalK.
+
+        int soma =0;
+        for(int cont=1; cont <=100;cont++){
+            soma+=cont;
+        }
+        System.out.println(soma);
+    }
+}
