1b6a34a768d4f7aae23870f9f144524b493f881c
YASMIN JHENIFER <155997837+yasminjheniferDev@users.noreply.github.com>
2026-09-05 12:10:33 -0300

    + 1 Exercício

---
+++ Exercicios 3/NDe1Ate20.java
@@ -0,0 +1,9 @@
+public class NDe1Ate20 {
+    public static void main(String[] args) {
+
+        // Laço imprimindo números de 1 até 20.
+        for( int cont = 1; cont <=20;cont++){
+            System.out.println(cont);
+        }
+    }
+}
